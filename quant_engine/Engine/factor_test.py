45bc928cc9559f79f8262beb675b491212476c8a
Gus <bright_gl@126.com>
2019-12-24 10:21:10 +0800

    增加了行业内等权和不等权两种模式 中性化由rlm改回ols

--- quant_engine/Engine/factor_test.py
+++ quant_engine/Engine/factor_test.py
@@ -68,8 +68,8 @@ class FactorTest:
             day_code       = processed_factor.loc[processed_factor['date']==date,'code']
             day_factor     = processed_factor.loc[processed_factor['date']==date,factor_field]
             day_idsty_size = processed_factor.loc[processed_factor['date']==date,idsty_size_cols]
-            RLM_est        = sm.RLM(day_factor, day_idsty_size, M=sm.robust.norms.HuberT()).fit()
-            day_orthed_factor = RLM_est.resid
+            OLS_est        = sm.OLS(day_factor, day_idsty_size).fit()
+            day_orthed_factor = OLS_est.resid
             day_orthed_factor.name = factor_field
             # 得到正交化后的因子值
             day_orthed_factor = pd.concat([day_code, day_orthed_factor], axis=1)
@@ -106,8 +106,9 @@ class FactorTest:
         return pd.Series(day_IC,index=IC_date)
 
 
+    # 等权所用的分组job
     @staticmethod
-    def job_group_weight(dates,groups,factor,factor_field):
+    def job_group_equal_weight(dates,groups,factor,factor_field):
         labels = []
         for i in range(1,groups+1):
             labels.append('group_'+str(i))
@@ -122,13 +123,41 @@ class FactorTest:
                     day_industry_factor['weight_in_industry'] = 100 / day_industry_factor.shape[0]
                     day_industry_factor['group'] = 'same group'
                 else:
-                    day_industry_factor['group'] = pd.qcut(day_industry_factor[factor_field],5,labels=labels)
+                    day_industry_factor['group'] = pd.qcut(day_industry_factor[factor_field], 5, labels=labels)
                     group_counts = day_industry_factor['group'].value_counts()
                     day_industry_factor['weight_in_industry'] = day_industry_factor.apply(
                         lambda row:100/group_counts[row['group']],axis=1)
                 res.append(day_industry_factor)
         res_df = pd.concat(res)
         res_df.set_index('next_1_day',inplace=True)
+        return res_df
+
+
+    # 市值加权所用的分组job
+    @staticmethod
+    def job_group_nonequal_weight(dates,groups,factor,factor_field):
+        labels = []
+        for i in range(1, groups + 1):
+            labels.append('group_' + str(i))
+        res = []
+        for date in dates:
+            day_factor = factor.loc[factor['date']==date,:].copy()
+            industries = day_factor['industry'].unique()
+            for ind in industries:
+                day_industry_factor = day_factor.loc[day_factor['industry']==ind,:].copy()
+                day_industry_size_sum = day_industry_factor['size'].sum()
+                # 行业成分不足10支票时，所有group配置一样
+                if day_industry_factor.shape[0] < 10:
+                    day_industry_factor['weight_in_industry'] = 100/day_industry_size_sum * day_industry_factor['size']
+                    day_industry_factor['group'] = 'same group'
+                else:
+                    day_industry_factor['group'] = pd.qcut(day_industry_factor[factor_field], 5, labels=labels)
+                    group_size_sum = day_industry_factor.groupby('group')['size'].sum()
+                    day_industry_factor['weight_in_industry'] = \
+                        day_industry_factor.apply(lambda row: 100/group_size_sum[row['group']]*row['size'], axis=1)
+                res.append(day_industry_factor)
+        res_df = pd.concat(res)
+        res_df.set_index('next_1_day', inplace=True)
         return res_df
 
 
@@ -297,7 +326,8 @@ class FactorTest:
         return IC
 
 
-    def group_factor(self,orth_factor,mkt_data):
+    # 此处weight_field为行业内权重分配的field
+    def group_factor(self,orth_factor,mkt_data,size_data,weight_field=None):
         benchmark_field = self.benchmark + '_weight'
         mkt_data.dropna(subset=[self.industry_field],inplace=True)
         mkt_data = mkt_data.loc[:,[benchmark_field,self.industry_field,'code','status']]
@@ -321,24 +351,47 @@ class FactorTest:
         mkt_data = pd.merge(mkt_data,industry_weight,on=['date','industry'])
         # 合并得到当天因子，行业，状态，下一交易日benchmark权重，下一交易日行业总权重
         orth_factor = pd.merge(orth_factor,mkt_data,on=['date','code'])
-        # 去掉当天停牌的股票
-        orth_factor = orth_factor.loc[~((orth_factor['status']=='停牌')|pd.isnull(orth_factor['status'])),
-                                      ['date','code','next_1_day',self.factor_field,'industry','industry_weight']]
-        dates = orth_factor['date'].unique()
-        split_dates = np.array_split(dates,30)
-        with parallel_backend('multiprocessing', n_jobs=4):
-            result_list =  Parallel()(delayed(FactorTest.job_group_weight)
-                                      (dates,self.groups,orth_factor,self.factor_field) for dates in split_dates)
-        grouped_weight = pd.concat(result_list)
-        grouped_weight = grouped_weight.sort_index()
-        grouped_weight = grouped_weight.loc[grouped_weight['weight_in_industry']>0,:]
-        filename = global_constant.ROOT_DIR + 'Backtest_Result/Factor_Group_Weight/' + \
-                   self.factor_field + '_' + str(self.groups) + 'groups' + '.csv'
-        grouped_weight.to_csv(filename,encoding='gbk')
+        # 如果没有size_field, 就等权
+        if not weight_field:
+            # 去掉当天停牌的股票
+            orth_factor = orth_factor.loc[~((orth_factor['status']=='停牌')|pd.isnull(orth_factor['status'])),
+                                          ['date','code','next_1_day',self.factor_field,'industry','industry_weight']]
+            dates = orth_factor['date'].unique()
+            split_dates = np.array_split(dates,30)
+            with parallel_backend('multiprocessing', n_jobs=4):
+                result_list = Parallel()(delayed(FactorTest.job_group_equal_weight)
+                                        (dates,self.groups,orth_factor,self.factor_field) for dates in split_dates)
+            grouped_weight = pd.concat(result_list)
+            grouped_weight = grouped_weight.loc[grouped_weight['weight_in_industry']>0,:].sort_index()
+            filename = global_constant.ROOT_DIR + 'Backtest_Result/Factor_Group_Weight/' + \
+                       self.factor_field + '_' + str(self.groups) + 'groups_equal' + '.csv'
+            grouped_weight.to_csv(filename,encoding='gbk')
+        # 如果有weight_field，按照市值加权
+        else:
+            size_data = size_data.loc[:,['code',weight_field]]
+            size_data.rename(columns={weight_field:'size'},inplace=True)
+            size_data.index.names = ['date']
+            size_data.reset_index(inplace=True)
+            orth_factor = pd.merge(orth_factor,size_data,on=['date','code'])
+            # 去掉当天停牌的股票
+            orth_factor = orth_factor.loc[~((orth_factor['status'] == '停牌') | pd.isnull(orth_factor['status'])),
+                                          ['date','code','next_1_day',self.factor_field,'size','industry','industry_weight']]
+            dates = orth_factor['date'].unique()
+            split_dates = np.array_split(dates, 30)
+            with parallel_backend('multiprocessing', n_jobs=4):
+                result_list = Parallel()(delayed(FactorTest.job_group_nonequal_weight)
+                                         (dates, self.groups, orth_factor, self.factor_field) for dates in split_dates)
+            grouped_weight = pd.concat(result_list)
+            grouped_weight = grouped_weight.sort_index()
+            grouped_weight = grouped_weight.loc[grouped_weight['weight_in_industry']>0, :]
+            filename = global_constant.ROOT_DIR + 'Backtest_Result/Factor_Group_Weight/' + \
+                       self.factor_field + '_' + str(self.groups) + 'groups_' + weight_field + '.csv'
+            grouped_weight.to_csv(filename, encoding='gbk')
         return grouped_weight
 
 
-    def group_backtest(self,grouped_weight):
+    def group_backtest(self,grouped_weight,weight_mode):
+        self.summary_dict['WeightMode'] = weight_mode
         start = grouped_weight.index.min().strftime('%Y%m%d')
         end = grouped_weight.index.max().strftime('%Y%m%d')
         bt_data = self.influx.getDataMultiprocess('DailyData_Gus','marketData',start,end)
@@ -360,7 +413,7 @@ class FactorTest:
         for col in index_value.columns:
             index_value[col] = index_value[col]/index_value[col].iloc[0] * self.capital
         tot_res = pd.merge(tot_res,index_value,left_index=True,right_index=True)
-        filename = global_constant.ROOT_DIR + '/Backtest_Result/Group_Value/' + self.save_name + '.csv'
+        filename = global_constant.ROOT_DIR +'/Backtest_Result/Group_Value/'+ self.save_name + '_' + weight_mode + '.csv'
         tot_res.to_csv(filename, encoding='gbk')
         # 计算指标
         self.summary_dict['TimePeriod'] = tot_res.index[0].strftime('%Y%m%d')+'~'+tot_res.index[-1].strftime('%Y%m%d')
@@ -382,16 +435,20 @@ class FactorTest:
         self.summary_dict['sharpe_alpha_300'] = DataProcess.calc_alpha_sharpe(tot_res[group[-1]+'_TotalValue'],tot_res['000300.SH'])
         self.summary_dict['sharpe_alpha_500'] = DataProcess.calc_alpha_sharpe(tot_res[group[-1]+'_TotalValue'],tot_res['000905.SH'])
         self.summary_dict['sharpe_alpha_800'] = DataProcess.calc_alpha_sharpe(tot_res[group[-1]+'_TotalValue'],tot_res['300+500'])
+        return self.summary_dict.copy()
 
-
-    def generate_report(self):
-        rep = {self.factor_field:self.summary_dict}
-        rep = pd.DataFrame(rep)
-        rep = rep.reindex(index=['TimePeriod','F_over_0_pct','avg_abs_T','abs_T_over_2_pct','IC_mean','IC_std',
-                                 'IC_over_0_pct','abs_IC_over_20pct_pct','IR','AnnRet_high_group','AnnRet_300',
-                                 'AnnRet_500','AnnRet_800','alpha_300','alpha_500','alpha_800','MDD_high_group',
-                                 'MDD_300','MDD_500','MDD_800','sharpe_high_group','sharpe_300','sharpe_500',
-                                 'sharpe_800','sharpe_alpha_300','sharpe_alpha_500','sharpe_alpha_800'])
+    def generate_report(self,*summary_dicts):
+        reps = []
+        for summary_dict in summary_dicts:
+            rep = {self.factor_field: summary_dict}
+            rep = pd.DataFrame(rep)
+            rep = rep.reindex(index=['TimePeriod','WeightMode','F_over_0_pct','avg_abs_T','abs_T_over_2_pct','IC_mean',
+                                     'IC_std','IC_over_0_pct','abs_IC_over_20pct_pct','IR','AnnRet_high_group',
+                                     'AnnRet_300','AnnRet_500','AnnRet_800','alpha_300','alpha_500','alpha_800',
+                                     'MDD_high_group','MDD_300','MDD_500','MDD_800','sharpe_high_group','sharpe_300',
+                                     'sharpe_500','sharpe_800','sharpe_alpha_300','sharpe_alpha_500','sharpe_alpha_800'])
+            reps.append(rep)
+        rep = pd.concat(reps,axis=1)
         filename = global_constant.ROOT_DIR + '/Backtest_Result/Factor_Report/' + self.save_name + '.csv'
         rep.to_csv(filename, encoding='gbk')
 
@@ -429,13 +486,15 @@ class FactorTest:
         self.validity_check(orth_factor,preprocessed_mkt_data)
         print('validity checking finish')
         print('-' * 30)
-        grouped_weight = self.group_factor(orth_factor,mkt_data)
+        grouped_equal_weight = self.group_factor(orth_factor,mkt_data,None,None)
+        grouped_nonequal_weight = self.group_factor(orth_factor,mkt_data,size_data,'market_cap')
         print('factor grouping finish')
         print('-' * 30)
-        self.group_backtest(grouped_weight)
+        summary1 = self.group_backtest(grouped_equal_weight,'equal')
+        summary2 = self.group_backtest(grouped_nonequal_weight,'market_cap')
         print('group backtest finish')
         print('-' * 30)
-        self.generate_report()
+        self.generate_report(summary1,summary2)
         print('report got')
 
 
@@ -445,11 +504,12 @@ if __name__ == '__main__':
     strategy = FactorTest()
 
     start = 20120101
-    end = 20131231
+    end = 20121231
     mkt_data = strategy.influx.getDataMultiprocess('DailyData_Gus', 'marketData', start, end, None)
     mkt_data = mkt_data.loc[pd.notnull(mkt_data['IF_weight'])|pd.notnull(mkt_data['IC_weight']),:]
     factor = strategy.influx.getDataMultiprocess('DailyFactor_Gus','Growth',start,end,['code','EPcut_TTM_growthY'])
     factor = factor.dropna(subset=['EPcut_TTM_growthY'])
     print('factor loaded!')
     size_data = strategy.influx.getDataMultiprocess('DailyFactor_Gus','Size',start,end,None)
-    strategy.run_factor_test(mkt_data,factor,'EPcut_TTM_growthY',size_data,'EPcut_TTM_growthY',benchmark='IF',mkt_cap_field='ln_market_cap')
+    strategy.run_factor_test(mkt_data,factor,'EPcut_TTM_growthY',size_data,'EPcut_TTM_growthY',benchmark='IF',
+                             mkt_cap_field='ln_market_cap')
