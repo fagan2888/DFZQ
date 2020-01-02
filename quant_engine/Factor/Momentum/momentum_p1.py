f0161df1f9c1b7e4f56405dd9adaf4a56cba9ad9
Gus <bright_gl@126.com>
2020-01-02 16:50:19 +0800

    除数为0时特殊处理

--- quant_engine/Factor/Momentum/momentum_p1.py
+++ quant_engine/Factor/Momentum/momentum_p1.py
@@ -35,25 +35,34 @@ class momentum_p1(FactorBase):
                 if period_m1_df.shape[0] < 10:
                     pass
                 else:
-                    period_m1_df['n_days_to_idx'] = range(period_m1_df.shape[0] - 1, -1, -1)
-                    period_m1_df['exp_wgt'] = period_m1_df['n_days_to_idx'].apply(lambda x: math.exp(x * -1 / 1 / 4))
                     rtn_m1 = (period_m1_df['close'].iloc[-1] * period_m1_df['adj_factor'].iloc[-1]) \
                              / (period_m1_df['preclose'].iloc[0] * period_m1_df['adj_factor'].iloc[0]) - 1
-                    wgt_rtn_m1 = \
-                        period_m1_df['wgt_return'].sum() / period_m1_df['turnover'].sum()
-                    float_wgt_rtn_m1 = \
-                        period_m1_df['float_wgt_return'].sum() / period_m1_df['float_turnover'].sum()
-                    free_wgt_rtn_m1 = \
-                        period_m1_df['free_wgt_return'].sum() / period_m1_df['free_turnover'].sum()
-                    exp_wgt_rtn_m1 = \
-                        (period_m1_df['return'] * period_m1_df['turnover'] * period_m1_df['exp_wgt']).sum() \
-                        / (period_m1_df['turnover'] * period_m1_df['exp_wgt']).sum()
-                    float_exp_wgt_rtn_m1 = \
-                        (period_m1_df['return'] * period_m1_df['float_turnover'] * period_m1_df['exp_wgt']).sum() \
-                        / (period_m1_df['float_turnover'] * period_m1_df['exp_wgt']).sum()
-                    free_exp_wgt_rtn_m1 = \
-                        (period_m1_df['return'] * period_m1_df['free_turnover'] * period_m1_df['exp_wgt']).sum() \
-                        / (period_m1_df['free_turnover'] * period_m1_df['exp_wgt']).sum()
+                    if period_m1_df['turnover'].sum() == 0:
+                        wgt_rtn_m1 = 0
+                        float_wgt_rtn_m1 = 0
+                        free_wgt_rtn_m1 = 0
+                        exp_wgt_rtn_m1 = 0
+                        float_exp_wgt_rtn_m1 = 0
+                        free_exp_wgt_rtn_m1 = 0
+                    else:
+                        period_m1_df['n_days_to_idx'] = range(period_m1_df.shape[0] - 1, -1, -1)
+                        period_m1_df['exp_wgt'] = period_m1_df['n_days_to_idx'].apply(
+                            lambda x: math.exp(x * -1 / 1 / 4))
+                        wgt_rtn_m1 = \
+                            period_m1_df['wgt_return'].sum() / period_m1_df['turnover'].sum()
+                        float_wgt_rtn_m1 = \
+                            period_m1_df['float_wgt_return'].sum() / period_m1_df['float_turnover'].sum()
+                        free_wgt_rtn_m1 = \
+                            period_m1_df['free_wgt_return'].sum() / period_m1_df['free_turnover'].sum()
+                        exp_wgt_rtn_m1 = \
+                            (period_m1_df['return'] * period_m1_df['turnover'] * period_m1_df['exp_wgt']).sum() \
+                            / (period_m1_df['turnover'] * period_m1_df['exp_wgt']).sum()
+                        float_exp_wgt_rtn_m1 = \
+                            (period_m1_df['return'] * period_m1_df['float_turnover'] * period_m1_df['exp_wgt']).sum() \
+                            / (period_m1_df['float_turnover'] * period_m1_df['exp_wgt']).sum()
+                        free_exp_wgt_rtn_m1 = \
+                            (period_m1_df['return'] * period_m1_df['free_turnover'] * period_m1_df['exp_wgt']).sum() \
+                            / (period_m1_df['free_turnover'] * period_m1_df['exp_wgt']).sum()
                     m1.append(pd.Series([idx, code, rtn_m1, wgt_rtn_m1, float_wgt_rtn_m1, free_wgt_rtn_m1,
                                          exp_wgt_rtn_m1, float_exp_wgt_rtn_m1, free_exp_wgt_rtn_m1],
                                         index=['date', 'code', 'rtn_m1', 'wgt_rtn_m1', 'float_wgt_rtn_m1',
@@ -64,25 +73,34 @@ class momentum_p1(FactorBase):
                 if period_m3_df.shape[0] < 30:
                     pass
                 else:
-                    period_m3_df['n_days_to_idx'] = range(period_m3_df.shape[0] - 1, -1, -1)
-                    period_m3_df['exp_wgt'] = period_m3_df['n_days_to_idx'].apply(lambda x: math.exp(x * -1 / 3 / 4))
                     rtn_m3 = (period_m3_df['close'].iloc[-1] * period_m3_df['adj_factor'].iloc[-1]) \
                              / (period_m3_df['preclose'].iloc[0] * period_m3_df['adj_factor'].iloc[0]) - 1
-                    wgt_rtn_m3 = \
-                        period_m3_df['wgt_return'].sum() / period_m3_df['turnover'].sum()
-                    float_wgt_rtn_m3 = \
-                        period_m3_df['float_wgt_return'].sum() / period_m3_df['float_turnover'].sum()
-                    free_wgt_rtn_m3 = \
-                        period_m3_df['free_wgt_return'].sum() / period_m3_df['free_turnover'].sum()
-                    exp_wgt_rtn_m3 = \
-                        (period_m3_df['return'] * period_m3_df['turnover'] * period_m3_df['exp_wgt']).sum() \
-                        / (period_m3_df['turnover'] * period_m3_df['exp_wgt']).sum()
-                    float_exp_wgt_rtn_m3 = \
-                        (period_m3_df['return'] * period_m3_df['float_turnover'] * period_m3_df['exp_wgt']).sum() \
-                        / (period_m3_df['float_turnover'] * period_m3_df['exp_wgt']).sum()
-                    free_exp_wgt_rtn_m3 = \
-                        (period_m3_df['return'] * period_m3_df['free_turnover'] * period_m3_df['exp_wgt']).sum() \
-                        / (period_m3_df['free_turnover'] * period_m3_df['exp_wgt']).sum()
+                    if period_m3_df['turnover'].sum() == 0:
+                        wgt_rtn_m3 = 0
+                        float_wgt_rtn_m3 = 0
+                        free_wgt_rtn_m3 = 0
+                        exp_wgt_rtn_m3 = 0
+                        float_exp_wgt_rtn_m3 = 0
+                        free_exp_wgt_rtn_m3 = 0
+                    else:
+                        period_m3_df['n_days_to_idx'] = range(period_m3_df.shape[0] - 1, -1, -1)
+                        period_m3_df['exp_wgt'] = period_m3_df['n_days_to_idx'].apply(
+                            lambda x: math.exp(x * -1 / 3 / 4))
+                        wgt_rtn_m3 = \
+                            period_m3_df['wgt_return'].sum() / period_m3_df['turnover'].sum()
+                        float_wgt_rtn_m3 = \
+                            period_m3_df['float_wgt_return'].sum() / period_m3_df['float_turnover'].sum()
+                        free_wgt_rtn_m3 = \
+                            period_m3_df['free_wgt_return'].sum() / period_m3_df['free_turnover'].sum()
+                        exp_wgt_rtn_m3 = \
+                            (period_m3_df['return'] * period_m3_df['turnover'] * period_m3_df['exp_wgt']).sum() \
+                            / (period_m3_df['turnover'] * period_m3_df['exp_wgt']).sum()
+                        float_exp_wgt_rtn_m3 = \
+                            (period_m3_df['return'] * period_m3_df['float_turnover'] * period_m3_df['exp_wgt']).sum() \
+                            / (period_m3_df['float_turnover'] * period_m3_df['exp_wgt']).sum()
+                        free_exp_wgt_rtn_m3 = \
+                            (period_m3_df['return'] * period_m3_df['free_turnover'] * period_m3_df['exp_wgt']).sum() \
+                            / (period_m3_df['free_turnover'] * period_m3_df['exp_wgt']).sum()
                     m3.append(pd.Series([idx, code, rtn_m3, wgt_rtn_m3, float_wgt_rtn_m3, free_wgt_rtn_m3,
                                          exp_wgt_rtn_m3, float_exp_wgt_rtn_m3, free_exp_wgt_rtn_m3],
                                         index=['date', 'code', 'rtn_m3', 'wgt_rtn_m3', 'float_wgt_rtn_m3',
@@ -93,25 +111,34 @@ class momentum_p1(FactorBase):
                 if period_m6_df.shape[0] < 60:
                     pass
                 else:
-                    period_m6_df['n_days_to_idx'] = range(period_m6_df.shape[0] - 1, -1, -1)
-                    period_m6_df['exp_wgt'] = period_m6_df['n_days_to_idx'].apply(lambda x: math.exp(x * -1 / 6 / 4))
                     rtn_m6 = (period_m6_df['close'].iloc[-1] * period_m6_df['adj_factor'].iloc[-1]) \
                              / (period_m6_df['preclose'].iloc[0] * period_m6_df['adj_factor'].iloc[0]) - 1
-                    wgt_rtn_m6 = \
-                        period_m6_df['wgt_return'].sum() / period_m6_df['turnover'].sum()
-                    float_wgt_rtn_m6 = \
-                        period_m6_df['float_wgt_return'].sum() / period_m6_df['float_turnover'].sum()
-                    free_wgt_rtn_m6 = \
-                        period_m6_df['free_wgt_return'].sum() / period_m6_df['free_turnover'].sum()
-                    exp_wgt_rtn_m6 = \
-                        (period_m6_df['return'] * period_m6_df['turnover'] * period_m6_df['exp_wgt']).sum() \
-                        / (period_m6_df['turnover'] * period_m6_df['exp_wgt']).sum()
-                    float_exp_wgt_rtn_m6 = \
-                        (period_m6_df['return'] * period_m6_df['float_turnover'] * period_m6_df['exp_wgt']).sum() \
-                        / (period_m6_df['float_turnover'] * period_m6_df['exp_wgt']).sum()
-                    free_exp_wgt_rtn_m6 = \
-                        (period_m6_df['return'] * period_m6_df['free_turnover'] * period_m6_df['exp_wgt']).sum() \
-                        / (period_m6_df['free_turnover'] * period_m6_df['exp_wgt']).sum()
+                    if period_m6_df['turnover'].sum() == 0:
+                        wgt_rtn_m6 = 0
+                        float_wgt_rtn_m6 = 0
+                        free_wgt_rtn_m6 = 0
+                        exp_wgt_rtn_m6 = 0
+                        float_exp_wgt_rtn_m6 = 0
+                        free_exp_wgt_rtn_m6 = 0
+                    else:
+                        period_m6_df['n_days_to_idx'] = range(period_m6_df.shape[0] - 1, -1, -1)
+                        period_m6_df['exp_wgt'] = period_m6_df['n_days_to_idx'].apply(
+                            lambda x: math.exp(x * -1 / 6 / 4))
+                        wgt_rtn_m6 = \
+                            period_m6_df['wgt_return'].sum() / period_m6_df['turnover'].sum()
+                        float_wgt_rtn_m6 = \
+                            period_m6_df['float_wgt_return'].sum() / period_m6_df['float_turnover'].sum()
+                        free_wgt_rtn_m6 = \
+                            period_m6_df['free_wgt_return'].sum() / period_m6_df['free_turnover'].sum()
+                        exp_wgt_rtn_m6 = \
+                            (period_m6_df['return'] * period_m6_df['turnover'] * period_m6_df['exp_wgt']).sum() \
+                            / (period_m6_df['turnover'] * period_m6_df['exp_wgt']).sum()
+                        float_exp_wgt_rtn_m6 = \
+                            (period_m6_df['return'] * period_m6_df['float_turnover'] * period_m6_df['exp_wgt']).sum() \
+                            / (period_m6_df['float_turnover'] * period_m6_df['exp_wgt']).sum()
+                        free_exp_wgt_rtn_m6 = \
+                            (period_m6_df['return'] * period_m6_df['free_turnover'] * period_m6_df['exp_wgt']).sum() \
+                            / (period_m6_df['free_turnover'] * period_m6_df['exp_wgt']).sum()
                     m6.append(pd.Series([idx, code, rtn_m6, wgt_rtn_m6, float_wgt_rtn_m6, free_wgt_rtn_m6,
                                          exp_wgt_rtn_m6, float_exp_wgt_rtn_m6, free_exp_wgt_rtn_m6],
                                         index=['date', 'code', 'rtn_m6', 'wgt_rtn_m6', 'float_wgt_rtn_m6',
@@ -122,25 +149,34 @@ class momentum_p1(FactorBase):
                 if period_m12_df.shape[0] < 120:
                     pass
                 else:
-                    period_m12_df['n_days_to_idx'] = range(period_m12_df.shape[0] - 1, -1, -1)
-                    period_m12_df['exp_wgt'] = period_m12_df['n_days_to_idx'].apply(lambda x: math.exp(x * -1 / 12 / 4))
                     rtn_m12 = (period_m12_df['close'].iloc[-1] * period_m12_df['adj_factor'].iloc[-1]) \
                               / (period_m12_df['preclose'].iloc[0] * period_m12_df['adj_factor'].iloc[0]) - 1
-                    wgt_rtn_m12 = \
-                        period_m12_df['wgt_return'].sum() / period_m12_df['turnover'].sum()
-                    float_wgt_rtn_m12 = \
-                        period_m12_df['float_wgt_return'].sum() / period_m12_df['float_turnover'].sum()
-                    free_wgt_rtn_m12 = \
-                        period_m12_df['free_wgt_return'].sum() / period_m12_df['free_turnover'].sum()
-                    exp_wgt_rtn_m12 = \
-                        (period_m12_df['return'] * period_m12_df['turnover'] * period_m12_df['exp_wgt']).sum() \
-                        / (period_m12_df['turnover'] * period_m12_df['exp_wgt']).sum()
-                    float_exp_wgt_rtn_m12 = \
-                        (period_m12_df['return'] * period_m12_df['float_turnover'] * period_m12_df['exp_wgt']).sum() \
-                        / (period_m12_df['float_turnover'] * period_m12_df['exp_wgt']).sum()
-                    free_exp_wgt_rtn_m12 = \
-                        (period_m12_df['return'] * period_m12_df['free_turnover'] * period_m12_df['exp_wgt']).sum() \
-                        / (period_m12_df['free_turnover'] * period_m12_df['exp_wgt']).sum()
+                    if period_m12_df['turnover'].sum() == 0:
+                        wgt_rtn_m12 = 0
+                        float_wgt_rtn_m12 = 0
+                        free_wgt_rtn_m12 = 0
+                        exp_wgt_rtn_m12 = 0
+                        float_exp_wgt_rtn_m12 = 0
+                        free_exp_wgt_rtn_m12 = 0
+                    else:
+                        period_m12_df['n_days_to_idx'] = range(period_m12_df.shape[0] - 1, -1, -1)
+                        period_m12_df['exp_wgt'] = period_m12_df['n_days_to_idx'].apply(
+                            lambda x: math.exp(x * -1 / 12 / 4))
+                        wgt_rtn_m12 = \
+                            period_m12_df['wgt_return'].sum() / period_m12_df['turnover'].sum()
+                        float_wgt_rtn_m12 = \
+                            period_m12_df['float_wgt_return'].sum() / period_m12_df['float_turnover'].sum()
+                        free_wgt_rtn_m12 = \
+                            period_m12_df['free_wgt_return'].sum() / period_m12_df['free_turnover'].sum()
+                        exp_wgt_rtn_m12 = \
+                            (period_m12_df['return'] * period_m12_df['turnover'] * period_m12_df['exp_wgt']).sum() \
+                            / (period_m12_df['turnover'] * period_m12_df['exp_wgt']).sum()
+                        float_exp_wgt_rtn_m12 = \
+                            (period_m12_df['return'] * period_m12_df['float_turnover'] * period_m12_df['exp_wgt']).sum() \
+                            / (period_m12_df['float_turnover'] * period_m12_df['exp_wgt']).sum()
+                        free_exp_wgt_rtn_m12 = \
+                            (period_m12_df['return'] * period_m12_df['free_turnover'] * period_m12_df['exp_wgt']).sum() \
+                            / (period_m12_df['free_turnover'] * period_m12_df['exp_wgt']).sum()
                     m12.append(pd.Series([idx, code, rtn_m12, wgt_rtn_m12, float_wgt_rtn_m12, free_wgt_rtn_m12,
                                           exp_wgt_rtn_m12, float_exp_wgt_rtn_m12, free_exp_wgt_rtn_m12],
                                          index=['date', 'code', 'rtn_m12', 'wgt_rtn_m12', 'float_wgt_rtn_m12',
