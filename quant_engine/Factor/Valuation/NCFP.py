a8bfed80e6e6fb44f42924efa86e2aea9bb74fc0
Gus <bright_gl@126.com>
2020-02-10 09:52:53 +0800

    新加因子NCFP,OCFP

---
+++ quant_engine/Factor/Valuation/NCFP.py
@@ -0,0 +1,49 @@
+from factor_base import FactorBase
+import pandas as pd
+import numpy as np
+from influxdb_data import influxdbData
+import datetime
+from joblib import Parallel, delayed, parallel_backend
+from global_constant import N_JOBS
+
+
+class NCFP(FactorBase):
+    def __init__(self):
+        super().__init__()
+        self.db = 'DailyFactors_Gus'
+        self.measure = 'NCFP'
+
+    def cal_factors(self, start, end, n_jobs):
+        net_CF = self.influx.getDataMultiprocess('FinancialReport_Gus', 'net_CF_TTM', start, end,
+                                                 ['code', 'net_CF_TTM'])
+        market_cap = self.influx.getDataMultiprocess('DailyFactors_Gus', 'Size', start, end,
+                                                     ['code', 'market_cap'])
+        net_CF.index.names = ['date']
+        net_CF.reset_index(inplace=True)
+        market_cap.index.names = ['date']
+        market_cap.reset_index(inplace=True)
+        # market_cap 的单位为万元
+        NCFP = pd.merge(net_CF, market_cap, on=['date', 'code'])
+        NCFP.set_index('date', inplace=True)
+        NCFP['NCFP'] = NCFP['net_CF_TTM'] / NCFP['market_cap'] / 10000
+        NCFP = NCFP.loc[:, ['code', 'NCFP']]
+        codes = NCFP['code'].unique()
+        split_codes = np.array_split(codes, n_jobs)
+        with parallel_backend('multiprocessing', n_jobs=n_jobs):
+            res = Parallel()(delayed(influxdbData.JOB_saveData)
+                             (NCFP, 'code', codes, self.db, self.measure) for codes in split_codes)
+        print('NCFP finish')
+        print('-' * 30)
+        fail_list = []
+        for r in res:
+            fail_list.extend(r)
+        return fail_list
+
+
+if __name__ == '__main__':
+    print(datetime.datetime.now())
+    NCFP = NCFP()
+    r = NCFP.cal_factors(20100101, 20200205, N_JOBS)
+    print('task finish')
+    print(r)
+    print(datetime.datetime.now())
---
+++ quant_engine/Factor/Valuation/OCFP.py
@@ -0,0 +1,49 @@
+from factor_base import FactorBase
+import pandas as pd
+import numpy as np
+from influxdb_data import influxdbData
+import datetime
+from joblib import Parallel, delayed, parallel_backend
+from global_constant import N_JOBS
+
+
+class OCFP(FactorBase):
+    def __init__(self):
+        super().__init__()
+        self.db = 'DailyFactors_Gus'
+        self.measure = 'OCFP'
+
+    def cal_factors(self, start, end, n_jobs):
+        net_OCF = self.influx.getDataMultiprocess('FinancialReport_Gus', 'net_OCF_TTM', start, end,
+                                                 ['code', 'net_OCF_TTM'])
+        market_cap = self.influx.getDataMultiprocess('DailyFactors_Gus', 'Size', start, end,
+                                                     ['code', 'market_cap'])
+        net_OCF.index.names = ['date']
+        net_OCF.reset_index(inplace=True)
+        market_cap.index.names = ['date']
+        market_cap.reset_index(inplace=True)
+        # market_cap 的单位为万元
+        OCFP = pd.merge(net_OCF, market_cap, on=['date', 'code'])
+        OCFP.set_index('date', inplace=True)
+        OCFP['OCFP'] = OCFP['net_OCF_TTM'] / OCFP['market_cap'] / 10000
+        OCFP = OCFP.loc[:, ['code', 'OCFP']]
+        codes = OCFP['code'].unique()
+        split_codes = np.array_split(codes, n_jobs)
+        with parallel_backend('multiprocessing', n_jobs=n_jobs):
+            res = Parallel()(delayed(influxdbData.JOB_saveData)
+                             (OCFP, 'code', codes, self.db, self.measure) for codes in split_codes)
+        print('OCFP finish')
+        print('-' * 30)
+        fail_list = []
+        for r in res:
+            fail_list.extend(r)
+        return fail_list
+
+
+if __name__ == '__main__':
+    print(datetime.datetime.now())
+    OCFP = OCFP()
+    r = OCFP.cal_factors(20100101, 20200205, N_JOBS)
+    print('task finish')
+    print(r)
+    print(datetime.datetime.now())
