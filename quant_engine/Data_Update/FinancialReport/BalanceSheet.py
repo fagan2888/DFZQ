847326b0b99c0d4b18dfab2ce1fd28e4620d0348
Gus <bright_gl@126.com>
2020-03-02 15:36:00 +0800

    增加noa

--- quant_engine/Data_Update/FinancialReport/BalanceSheet.py
+++ quant_engine/Data_Update/FinancialReport/BalanceSheet.py
@@ -64,7 +64,10 @@ class BalanceSheetUpdate(FactorBase):
         # type要用408001000，408005000，408004000(合并报表，合并更正前，合并调整后)，同时有408001000和408005000用408005000
         # 有408004000时，根据ann_dt酌情使用
         query = "select ANN_DT, S_INFO_WINDCODE, REPORT_PERIOD, TOT_ASSETS, TOT_LIAB, " \
-                "TOT_SHRHLDR_EQY_EXCL_MIN_INT, STATEMENT_TYPE " \
+                "TOT_SHRHLDR_EQY_EXCL_MIN_INT, TOT_SHRHLDR_EQY_INCL_MIN_INT, ST_BORROW, TRADABLE_FIN_LIAB, " \
+                "NOTES_PAYABLE, NON_CUR_LIAB_DUE_WITHIN_1Y, LT_BORROW, BONDS_PAYABLE, " \
+                "MONETARY_CAP, TRADABLE_FIN_ASSETS, FIN_ASSETS_AVAIL_FOR_SALE, HELD_TO_MTY_INVEST, " \
+                "INVEST_REAL_ESTATE, TIME_DEPOSITS, OTH_ASSETS, LONG_TERM_REC, STATEMENT_TYPE " \
                 "from wind_filesync.AShareBalanceSheet " \
                 "where ANN_DT >= {0} and ANN_DT <= {1} " \
                 "and (STATEMENT_TYPE = '408001000' or STATEMENT_TYPE = '408005000' or STATEMENT_TYPE = '408004000') " \
@@ -72,21 +75,35 @@ class BalanceSheetUpdate(FactorBase):
                 "order by report_period, ann_dt, statement_type " \
             .format((dtparser.parse(str(start)) - relativedelta(years=2)).strftime('%Y%m%d'), str(end))
         self.rdf.curs.execute(query)
-        balance_sheet = pd.DataFrame(self.rdf.curs.fetchall(),
-                                     columns=['date', 'code', 'report_period', 'tot_assets', 'tot_liab',
-                                              'net_equity', 'type'])
+        balance_sheet = \
+            pd.DataFrame(self.rdf.curs.fetchall(),
+                         columns=['date', 'code', 'report_period', 'tot_assets', 'tot_liab', 'net_equity',
+                                  'net_equity_incl_min', 'shortterm_borrow', 'tradable_fin_liab', 'notes_payable',
+                                  'non_cur_liab_within1Y', 'longterm_borrow', 'bonds_payable', 'monetary_cap',
+                                  'tradable_fin_assets', 'fin_assets_avail_for_sale', 'held_to_maturity_invest',
+                                  'invest_real_estate', 'time_deposits', 'other_assets', 'longterm_rec', 'type'])
         # 同一code，同一date，同一report_period，同时出现type1，2，3时，取type大的
+        balance_sheet = balance_sheet.fillna(0)
         balance_sheet['type'] = \
             balance_sheet['type'].apply(lambda x: '1' if x == '408001000' else ('2' if x == '408005000' else '3'))
         balance_sheet = balance_sheet.sort_values(by=['code', 'date', 'report_period', 'type'])
         balance_sheet['date'] = pd.to_datetime(balance_sheet['date'])
         balance_sheet['report_period'] = pd.to_datetime((balance_sheet['report_period']))
+        balance_sheet['NOA'] = balance_sheet['net_equity_incl_min'] + balance_sheet['shortterm_borrow'] + \
+                               balance_sheet['tradable_fin_liab'] + balance_sheet['notes_payable'] + \
+                               balance_sheet['non_cur_liab_within1Y'] + balance_sheet['longterm_borrow'] + \
+                               balance_sheet['bonds_payable'] - balance_sheet['monetary_cap'] - \
+                               balance_sheet['tradable_fin_assets'] - balance_sheet['fin_assets_avail_for_sale'] - \
+                               balance_sheet['held_to_maturity_invest'] - balance_sheet['invest_real_estate'] - \
+                               balance_sheet['time_deposits'] - balance_sheet['other_assets'] - \
+                               balance_sheet['longterm_rec']
+
         # 处理数据
         calendar = self.rdf.get_trading_calendar()
         calendar = \
             set(calendar.loc[(calendar >= (dtparser.parse(str(start)) - relativedelta(years=2)).strftime('%Y%m%d')) &
                              (calendar <= str(end))])
-        fields = balance_sheet.columns.difference(['date', 'code', 'report_period', 'type'])
+        fields = ['NOA', 'tot_assets', 'tot_liab', 'net_equity']
         fail_list = []
         for f in fields:
             print('field: %s begins processing...' %f)
@@ -111,4 +128,4 @@ class BalanceSheetUpdate(FactorBase):
 
 if __name__ == '__main__':
     BU = BalanceSheetUpdate()
-    r = BU.cal_factors(20190101, 20190901, N_JOBS)
+    r = BU.cal_factors(20100101, 20200225, N_JOBS)
