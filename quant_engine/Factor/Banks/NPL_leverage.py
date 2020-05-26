8ef452202f3a72e590f136955db9a4a47aee0be6
Gus <bright_gl@126.com>
2020-05-26 14:27:41 +0800

    重写，考虑了report_period的因素

--- quant_engine/Factor/Banks/NPL_leverage.py
+++ quant_engine/Factor/Banks/NPL_leverage.py
@@ -12,22 +12,44 @@ class NPL_leverage(FactorBase):
         self.db = 'DailyFactors_Gus'
         self.measure = 'NPL_leverage'
 
+    @staticmethod
+    def JOB_factors(df, codes, save_db, save_msr):
+        influx = influxdbData()
+        save_res = []
+        for code in codes:
+            code_df = df.loc[df['code'] == code, :].copy()
+            code_df = code_df.sort_values(['date', 'report_period'])
+            code_df['CA_ratio'] = code_df['CA_ratio'].fillna(method='ffill')
+            code_df['CA_ratio_last1Q'] = code_df['CA_ratio_last1Q'].fillna(method='ffill')
+            code_df = code_df.drop_duplicates(['date'], 'last')
+            code_df['NPL_leverage'] = code_df['NPL'] / code_df['CA_ratio']
+            code_df['NPL_leverage_last1Q'] = code_df['NPL_last1Q'] / code_df['CA_ratio_last1Q']
+            code_df = code_df.loc[:, ['date', 'code', 'report_period', 'NPL_leverage', 'NPL_leverage_last1Q']]
+            code_df.set_index('date', inplace=True)
+            code_df = code_df.where(pd.notnull(code_df), None)
+            print('code: %s' % code)
+            r = influx.saveData(code_df, save_db, save_msr)
+            if r == 'No error occurred...':
+                pass
+            else:
+                save_res.append('NPL_leverage Error: %s' % r)
+        return save_res
+
     def cal_factors(self, start, end, n_jobs):
-        NPL = self.influx.getDataMultiprocess('DailyFactors_Gus', 'NPL', start, end, ['code', 'NPL'])
-        CA_ratio = self.influx.getDataMultiprocess('DailyFactors_Gus', 'CA_ratio', start, end, ['code', 'CA_ratio'])
+        NPL = self.influx.getDataMultiprocess('DailyFactors_Gus', 'NPL', start, end,
+                                              ['code', 'NPL', 'NPL_last1Q', 'report_period'])
+        CA_ratio = self.influx.getDataMultiprocess('DailyFactors_Gus', 'CA_ratio', start, end,
+                                                   ['code', 'CA_ratio', 'CA_ratio_last1Q', 'report_period'])
         NPL.index.names = ['date']
         NPL.reset_index(inplace=True)
         CA_ratio.index.names = ['date']
         CA_ratio.reset_index(inplace=True)
-        NPL_leverage = pd.merge(NPL, CA_ratio, on=['date', 'code'])
-        NPL_leverage.set_index('date', inplace=True)
-        NPL_leverage['NPL_leverage'] = NPL_leverage['NPL'] / NPL_leverage['CA_ratio']
-        NPL_leverage = NPL_leverage.loc[:, ['code', 'NPL_leverage']]
-        codes = NPL_leverage['code'].unique()
+        merge_df = pd.merge(NPL, CA_ratio, how='outer', on=['date', 'code', 'report_period'])
+        codes = merge_df['code'].unique()
         split_codes = np.array_split(codes, n_jobs)
         with parallel_backend('multiprocessing', n_jobs=n_jobs):
-            res = Parallel()(delayed(influxdbData.JOB_saveData)
-                             (NPL_leverage, 'code', codes, self.db, self.measure) for codes in split_codes)
+            res = Parallel()(delayed(NPL_leverage.JOB_factors)
+                             (merge_df, codes, self.db, self.measure) for codes in split_codes)
         print('NPL_leverage finish')
         print('-' * 30)
         fail_list = []
@@ -38,7 +60,7 @@ class NPL_leverage(FactorBase):
 if __name__ == '__main__':
     print(datetime.datetime.now())
     NPL_leverage = NPL_leverage()
-    r = NPL_leverage.cal_factors(20100101, 20200520, N_JOBS)
+    r = NPL_leverage.cal_factors(20100101, 20200522, N_JOBS)
     print('task finish')
     print(r)
     print(datetime.datetime.now())
