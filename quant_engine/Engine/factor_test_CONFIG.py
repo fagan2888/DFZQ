982eb339d3bc3d71897aafd141c53eb80bf2f7c1
Gus <bright_gl@126.com>
2020-07-07 17:05:05 +0800

    factor_test_CONFIG

--- quant_engine/Engine/factor_test_CONFIG.py
+++ quant_engine/Engine/factor_test_CONFIG.py
@@ -3,18 +3,17 @@ from global_constant import FACTOR_DB, REPORT_DB
 TEST_CONFIG = \
     {
         'start': 20100101,
-        'end': 20181231,
+        'end': 20200701,
         'benchmark': 300,
         'select_range': 800,
         'industry': 'improved_lv1',
-        'size_field': 'ln_market_cap',
         'adj_interval': 5
     }
 
 # 大类因子权重
 CATEGORY_WEIGHT = \
     {
-        'GROWTH': 1,
+        'ANALYST': 1,
     }
 
 # 因子权重
@@ -28,6 +27,6 @@ FACTOR_WEIGHT = \
             [FACTOR_DB, 'ROE2', 'ROE', 1, 'median', 1, True]],
         'GROWTH': [
             [FACTOR_DB, 'net_profit_Q_growth', 'net_profit_Q_growthY', 1, 'median', 1, True, True]],
-        'BANK': [
-            [REPORT_DB, 'NPL', 'NPL', -1, 'median', 1, True]]
+        'ANALYST': [
+            [FACTOR_DB, 'PEG', 'PEG2', -1, None, 1, False, True]]
     }
